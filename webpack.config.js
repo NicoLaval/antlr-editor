e50fbb47e6b9c4b5f1748f898fc8ca4b48cb135c
NicoLaval <nicolas.laval@insee.fr>
2021-08-24 15:58:30 +0200

    Downgrade temp to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ const path = require("path");
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
-const ASSET_PATH = process.env.ASSET_PATH || "/";
-
 module.exports = {
     mode: "production",
     entry: "./src/index.ts",
@@ -42,7 +40,6 @@ module.exports = {
     output: {
         filename: "index.js",
         path: path.resolve(__dirname, "dist"),
-        publicPath: ASSET_PATH,
         library: {
             type: "commonjs2",
         },
